fb04eeef4e932c86108ffd5d7818c7b792db8335
cgrove8323 <cgrove8323@greenvilleschools.us>
2016-12-15 15:56:31 -0500

    Add files via upload

--- maze.py
+++ maze.py
@@ -25,6 +25,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
+BLUE = (31, 94, 196)
 
 # Fonts
 MY_FONT = pygame.font.Font(None, 50)
@@ -35,19 +36,33 @@ PLAYING = 1
 END = 2
 
 def setup():
-    global block_pos, block_vel, size, stage
-
-    block_pos = [375, 275]
-    block_vel = [0, 0]
-    size = 50
+    global coins, win, lose, stage, player, player_vx, player_vy, player_speed, badguy, badguy_vx, badguy_vy, badguy_speed 
 
     stage = START
+    win = False
+    lose = False
 
-# Make a player
-player =  [100, 0, 25, 25]
-player_vx = 0
-player_vy = 0
-player_speed = 5
+    # Make a player
+    player =  [525, 425, 25, 25]
+    player_vx = 0
+    player_vy = 0
+    player_speed = 5
+
+    # Make a Bad Guy
+    badguy = [100, 0, 25, 25]
+    badguy_vx = 0
+    badguy_vy = 0
+    badguy_speed = 5
+
+    # Make coins
+    coin1 = [325, 500, 25, 25]
+    coin2 = [400, 175, 25, 25]
+    coin3 = [150, 150, 25, 25]
+    coin4 = [150, 25, 25, 25]
+    coin5 = [625, 75, 25, 25]
+    coin6 = [475, 275, 25, 25]
+
+    coins = [coin1, coin2, coin3, coin4, coin5, coin6]
 
 # make walls
 wall1 = [75, 0, 25, 100]
@@ -101,6 +116,41 @@ wall48 = [500, 0, 175, 25]
 wall49 = [500, 100, 150, 25]
 wall50 = [650, 50, 25, 75]
 wall51 = [525, 50, 125, 25]
+wall52 = [700, 0, 25, 125]
+wall53 = [725, 0, 325, 25]
+wall54 = [1025, 25, 25, 50]
+wall55 = [725, 50, 125, 25]
+wall56 = [825, 75, 25, 50]
+wall57 = [725, 100, 25, 75]
+wall58 = [775, 100, 25, 50]
+wall59 = [800, 125, 75, 25]
+wall60 = [725, 175, 200, 25]
+wall61 = [875, 50, 175, 25]
+wall62 = [900, 100, 25, 75]
+wall63 = [725, 700, 25, 100]
+wall64 = [750, 750, 225, 25]
+wall65 = [950, 775, 25, 25]
+wall66 = [1075, 0, 100, 25]
+wall67 = [1175, 0, 25, 100]
+wall68 = [1075, 50, 75, 25]
+wall69 = [1175, 125, 25, 250]
+wall70 = [1000, 300, 175, 25]
+wall71 = [1000, 325, 25, 25]
+wall72 = [1150, 350, 25, 200]
+wall73 = [1175, 525, 25, 25]
+wall74 = [1050, 525, 25, 75]
+wall75 = [1075, 525, 50, 25]
+wall76 = [1100, 550, 25, 50]
+wall77 = [1125, 575, 75, 25]
+wall78 = [1150, 625, 25, 100]
+wall79 = [1175, 625, 25, 25]
+wall80 = [1175, 700, 25, 25]
+wall81 = [1000, 750, 200, 25]
+wall82 = [1000, 775, 25, 25]
+wall83 = [350, 50, 25, 50]
+wall84 = [150, 0, 250, 25]
+wall85 = [275, 50, 75, 25]
+wall86 = [275, 75, 25, 50]
 
 wall101 = [125, 0, 25, 75]
 wall102 = [150, 50, 100, 25]
@@ -158,13 +208,63 @@ wall153 = [650, 350, 50, 25]
 wall154 = [650, 400, 50, 25]
 wall155 = [650, 425, 25, 75]
 wall156 = [625, 500, 125, 25]
+wall157 = [700, 275, 25, 25]
+wall158 = [700, 250, 50, 25]
+wall159 = [725, 225, 175, 25]
+wall160 = [900, 225, 25, 125]
+wall161 = [775, 275, 100, 25]
+wall162 = [675, 300, 125, 25]
+wall163 = [825, 325, 75, 25]
+wall164 = [800, 350, 25, 75]
+wall165 = [725, 350, 75, 25]
+wall166 = [725, 375, 25, 100]
+wall167 = [700, 450, 25, 25]
+wall168 = [775, 400, 25, 150]
+wall169 = [950, 100, 25, 175]
+wall170 = [975, 250, 175, 25]
+wall171 = [1000, 100, 125, 25]
+wall172 = [1000, 125, 25, 100]
+wall173 = [1025, 200, 125, 25]
+wall174 = [1125, 100, 25, 50]
+wall175 = [1050, 150, 100, 25]
+wall176 = [950, 300, 25, 75]
+wall177 = [925, 375, 75, 25]
+wall178 = [900, 375, 25, 75]
+wall179 = [1000, 375, 25, 50]
+wall180 = [850, 375, 25, 75]
+wall181 = [825, 450, 100, 25]
+wall182 = [1050, 350, 25, 75]
+wall183 = [1100, 350, 25, 75]
+wall184 = [975, 425, 150, 25]
+wall185 = [950, 425, 25, 75]
+wall186 = [850, 500, 125, 25]
+wall187 = [825, 500, 25, 75]
+wall188 = [775, 575, 75, 25]
+wall189 = [775, 600, 25, 125]
+wall190 = [800, 700, 100, 25]
+wall191 = [825, 625, 25, 75]
+wall192 = [875, 600, 25, 100]
+wall193 = [875, 550, 75, 25]
+wall194 = [925, 575, 25, 75]
+wall195 = [975, 550, 25, 125]
+wall196 = [1000, 475, 25, 100]
+wall197 = [1025, 475, 100, 25]
+wall198 = [925, 675, 25, 50]
+wall199 = [950, 700, 150, 25]
+wall200 = [1100, 675, 25, 50]
+wall201 = [1025, 625, 25, 75]
+wall202 = [1050, 625, 75, 25]
 
 walls = [wall1, wall2, wall3, wall4, wall5, wall6, wall7,
          wall8, wall9, wall10, wall11,wall12, wall13, wall14, wall15, wall16,
          wall17, wall18, wall19, wall20, wall21, wall22, wall23, wall24, wall25,
          wall26, wall27, wall28, wall29, wall30, wall31, wall32, wall33, wall34,
          wall35, wall36, wall37, wall38, wall39, wall40, wall41, wall42, wall43,
-         wall44, wall45, wall46, wall47, wall48, wall49, wall50, wall51, 
+         wall44, wall45, wall46, wall47, wall48, wall49, wall50, wall51, wall52,
+         wall53, wall54, wall55, wall56, wall57, wall58, wall59, wall60, wall61,
+         wall62, wall63, wall64, wall65, wall66, wall67, wall68, wall69, wall70,
+         wall71, wall72, wall73, wall74, wall75, wall76, wall77, wall78, wall79,
+         wall80, wall81, wall82, wall83, wall84, wall85, wall86, 
          wall101, wall102, wall103, wall104, wall105,
          wall106, wall107, wall108, wall109, wall110, wall111,
          wall112, wall113, wall114, wall115, wall116, wall117, wall118, wall119,
@@ -172,24 +272,27 @@ walls = [wall1, wall2, wall3, wall4, wall5, wall6, wall7,
          wall128, wall129, wall130, wall131, wall132, wall133, wall134, wall135,
          wall136, wall137, wall138, wall139, wall140, wall141, wall142, wall143,
          wall144, wall145, wall146, wall147, wall148, wall149, wall150, wall151,
-         wall152, wall153, wall154, wall155, wall156]
+         wall152, wall153, wall154, wall155, wall156, wall157, wall158, wall159,
+         wall160, wall161, wall162, wall163, wall164, wall165, wall166, wall167,
+         wall168, wall169, wall170, wall171, wall172, wall173, wall174, wall175,
+         wall176, wall177, wall178, wall179, wall180, wall181, wall182, wall183,
+         wall184, wall185, wall186, wall187, wall188, wall189, wall190, wall191,
+         wall192, wall193, wall194, wall195, wall196, wall197, wall198, wall199,
+         wall200, wall201, wall202]
 
-# Make coins
-coin1 = [325, 500, 25, 25]
-coin2 = [400, 175, 25, 25]
-coin3 = [150, 150, 25, 25]
-
-coins = [coin1, coin2, coin3]
 
 
 # Game loop
 setup()
-win = False
 done = False
 
 while not done:
     # Event processing (React to key presses, mouse clicks, etc.)
     ''' for now, we'll just check to see if the X is clicked '''
+    
+    score = 0
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -205,31 +308,50 @@ while not done:
 
         pressed = pygame.key.get_pressed()
 
-        up = pressed[pygame.K_UP]
-        down = pressed[pygame.K_DOWN]
-        left = pressed[pygame.K_LEFT]
-        right = pressed[pygame.K_RIGHT]
+        player_up = pressed[pygame.K_UP]
+        player_down = pressed[pygame.K_DOWN]
+        player_left = pressed[pygame.K_LEFT]
+        player_right = pressed[pygame.K_RIGHT]
 
-        if up:
+        badguy_up = pressed[pygame.K_w]
+        badguy_down = pressed[pygame.K_s]
+        badguy_left = pressed[pygame.K_a]
+        badguy_right = pressed[pygame.K_d]
+
+        if player_up:
             player_vy = -player_speed
-        elif down:
+        elif player_down:
             player_vy = player_speed
         else:
             player_vy = 0
             
-        if left:
+        if player_left:
             player_vx = -player_speed
-        elif right:
+        elif player_right:
             player_vx = player_speed
         else:
             player_vx = 0
+
+        if badguy_up:
+            badguy_vy = -badguy_speed
+        elif badguy_down:
+            badguy_vy = badguy_speed
+        else:
+            badguy_vy = 0
+
+        if badguy_left:
+            badguy_vx = -badguy_speed
+        elif badguy_right:
+            badguy_vx = badguy_speed
+        else:
+            badguy_vx = 0
 
         
     # Game logic (Check for collisions, update points, etc.)
     if stage == PLAYING:
         ''' move the player in horizontal direction '''
         player[0] += player_vx
-
+        badguy[0] += badguy_vx
         ''' resolve collisions horizontally '''
         for w in walls:
             if intersects.rect_rect(player, w):        
@@ -238,9 +360,16 @@ while not done:
                 elif player_vx < 0:
                     player[0] = w[0] + w[2]
 
+        for w in walls:
+            if intersects.rect_rect(badguy, w):
+                if badguy_vx > 0:
+                    badguy[0] = w[0] - badguy[2]
+                elif badguy_vx < 0:
+                    badguy[0] = w[0] + w[2]
+
         ''' move the player in vertical direction '''
         player[1] += player_vy
-        
+        badguy[1] += badguy_vy
         ''' resolve collisions vertically '''
         for w in walls:
             if intersects.rect_rect(player, w):                    
@@ -248,6 +377,17 @@ while not done:
                     player[1] = w[1] - player[3]
                 if player_vy < 0:
                     player[1] = w[1] + w[3]
+
+        for w in walls:
+            if intersects.rect_rect(badguy, w):                    
+                if badguy_vy > 0:
+                    badguy[1] = w[1] - badguy[3]
+                if badguy_vy < 0:
+                    badguy[1] = w[1] + w[3]
+
+        if intersects.rect_rect(player, badguy):
+            stage = END
+            lose = True
 
 
     ''' here is where you should resolve player collisions with screen edges '''
@@ -259,13 +399,26 @@ while not done:
         player[1] = 0
     if player[1] + player[3] > HEIGHT:
         player[1] = HEIGHT - player[3]
-    
+
+    if badguy[0] < 0:
+        badguy[0] = 0
+    if badguy[0] + badguy[2] > WIDTH:
+        badguy[0] = WIDTH - badguy[2]
+    if badguy[1] < 0:
+        badguy[1] = 0
+    if badguy[1] + badguy[3] > HEIGHT:
+        badguy[1] = HEIGHT - badguy[3]
 
 
     ''' get the coins '''
-    coins = [c for c in coins if not intersects.rect_rect(player, c)]
+    hit_list = [c for c in coins if intersects.rect_rect(player, c)]
 
-    if len(coins) == 0:
+    for hit in hit_list:
+        coins.remove(hit)
+        score += 1
+        
+    if len(hit_list) == len(coins) + 1:
+        stage = END
         win = True
 
         
@@ -273,7 +426,7 @@ while not done:
     screen.fill(BLACK)
 
     pygame.draw.rect(screen, WHITE, player)
-   
+    pygame.draw.rect(screen, BLUE, badguy)
     
     for w in walls:
         pygame.draw.rect(screen, RED, w)
@@ -281,10 +434,7 @@ while not done:
     for c in coins:
         pygame.draw.rect(screen, YELLOW, c)
         
-    if win:
-        font = pygame.font.Font(None, 48)
-        text = font.render("You Win!", 1, GREEN)
-        screen.blit(text, [500, 200])
+   
 
     '''begin/end game text'''
     if stage == START:
@@ -292,11 +442,24 @@ while not done:
         text2 = MY_FONT.render("Press ENTER to play!", True, WHITE)
         screen.blit(text1, [400, 400])
         screen.blit(text2, [400, 500])
-    elif stage == END:
-        text1 = MY_FONT.render("Game Over", True, WHITE)
-        text2 = MY_FONT.render("Press SPACE to restart.", True, WHITE)
-        screen.blit(text1, [400, 400])
-        screen.blit(text2, [400, 500])
+    if stage == PLAYING:
+        font = pygame.font.Font(None, 30)
+        text1 = font.render("Score: " + str(score), True, WHITE)
+        screen.blit(text1, [1050, 775])
+    if stage == END:
+        text1 = MY_FONT.render("Press SPACE to restart.", True, WHITE)
+        screen.blit(text1, [400, 500])
+            
+        if win:
+            player_speed = 0
+            badguy_speed = 0
+            font = pygame.font.Font(None, 48)
+            text = font.render("YOU WIN!", 1, GREEN)
+            screen.blit(text, [500, 200])
+        if lose:
+            font = pygame.font.Font(None, 48)
+            text = font.render("YOU LOSE", 1, GREEN)
+            screen.blit(text, [500, 200])
 
     
     # Update screen (Actually draw the picture in the window.)
